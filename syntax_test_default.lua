be26938b8d79e7a6056edcdb47fcf046bbf53c28
MemoryPenguin <thecrazyp3nguin+git@gmail.com>
2016-08-29 20:58:00 -0400

    Initial commit

---
+++ syntax_test_default.lua
@@ -0,0 +1,21 @@
+-- SYNTAX TEST "Packages/RbxLua/rbxlua.sublime-syntax"
+0x0
+-- <- constant.numeric.hex.rbxlua
+
+0
+-- <- constant.numeric.rbxlua
+
+1e9
+-- <- constant.numeric.scientific.rbxlua
+
+workspace
+-- <- variable.language.rbxlua
+
+script
+-- <- variable.language.rbxlua
+
+plugin
+-- <- variable.language.rbxlua
+
+game
+-- <- variable.langauge.rbxlua
