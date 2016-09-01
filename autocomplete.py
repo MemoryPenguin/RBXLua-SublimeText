b64de2168a5d7a322db969f065be0a811fc3449d
MemoryPenguin <thecrazyp3nguin+git@gmail.com>
2016-09-01 17:52:15 -0400

    Enum and EnumItem completions!

--- autocomplete.py
+++ autocomplete.py
@@ -5,11 +5,13 @@ from ROBLOXLua.apiparser import parse_api_dump
 completion_items = parse_api_dump()
 
 FUNCTION_CALL_REGEX = re.compile(r"\b([\w\.]+)\s*\(?[\"\']")
+ENUM_REGEX = re.compile(r"\bEnum\.(\w*)(\.?\w*)")
 
 service_detections = [ "GetService", "FindService", "getService", "service" ]
 creatable_detections = [ "Instance.new" ]
 services = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Class" and "service" in e["entry_tags"] ])
 creatables = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Class" and "notCreatable" not in e["entry_tags"] and "abstract" not in e["entry_tags"] and "service" not in e["entry_tags"] and "deprecated" not in e["entry_tags"] ])
+enum_names = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Enum" ])
 
 class AutoCompleteProvider(sublime_plugin.EventListener):
 	"""
@@ -26,15 +28,20 @@ class AutoCompleteProvider(sublime_plugin.EventListener):
 			line_region = view.line(point)
 			line_text = view.substr(line_region)
 			function_match = FUNCTION_CALL_REGEX.search(line_text, 0, row_col[1])
+			enum_match = ENUM_REGEX.search(line_text, 0, row_col[1])
 
 			if function_match is not None and function_match.end(0) >= row_col[1]:
-				print(function_match.end(0), row_col[1])
 				function_name = function_match.group(1)
 
 				if function_name in service_detections:
 					selected_completions.update(services)
 				elif function_name in creatable_detections:
 					selected_completions.update(creatables)
+			elif enum_match is not None and enum_match.end(0) >= row_col[1]:
+				if enum_match.group(2) is None or enum_match.group(2) == "":
+					selected_completions.update(enum_names)
+				else:
+					selected_completions.update(set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "EnumItem" and e["enum_parent"] == enum_match.group(1) ]))
 
 		if len(selected_completions) > 0:
 			return ([ [e] for e in selected_completions ], sublime.INHIBIT_EXPLICIT_COMPLETIONS | sublime.INHIBIT_WORD_COMPLETIONS)
