c9d0817af89bd67faa12c51d394adaabf6299d5f
MemoryPenguin <thecrazyp3nguin+git@gmail.com>
2016-12-10 13:15:41 -0500

    Make function completions actually useful

--- autocomplete.py
+++ autocomplete.py
@@ -4,12 +4,13 @@ from ROBLOXLua.apiparser import parse_api_dump
 
 completion_items = parse_api_dump()
 
-FUNCTION_CALL_REGEX = re.compile(r"\b([\w\.]+)\s*\(?[\"\']")
+FUNCTION_CALL_REGEX = re.compile(r"\b(\w+)\s*\(?([\"\'])(\w*)\2?\)?")
 ENUM_REGEX = re.compile(r"\bEnum\.(\w*)([\.:]?\w*)")
 
 service_detections = [ "GetService", "FindService", "getService", "service" ]
 creatable_detections = [ "Instance.new" ]
 class_detections = [ "FindFirstOfClass" ]
+classes = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Class" ])
 services = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Class" and "service" in e["entry_tags"] ])
 creatables = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Class" and "notCreatable" not in e["entry_tags"] and "abstract" not in e["entry_tags"] and "service" not in e["entry_tags"] and "deprecated" not in e["entry_tags"] ])
 enum_names = set([ e["entry_completion"] for e in completion_items if e["entry_type"] == "Enum" ])
@@ -23,7 +24,7 @@ class AutoCompleteProvider(sublime_plugin.EventListener):
 
 		for point in points:
 			if "source.rbxlua" not in view.scope_name(point):
-				return
+				return None
 
 			row_col = view.rowcol(point)
 			line_region = view.line(point)
@@ -33,13 +34,17 @@ class AutoCompleteProvider(sublime_plugin.EventListener):
 
 			if function_match is not None and function_match.end(0) >= row_col[1]:
 				function_name = function_match.group(1)
+				value = None
 
 				if function_name in service_detections:
-					selected_completions.update(services)
+					value = services
 				elif function_name in creatable_detections:
-					selected_completions.update(creatables)
+					value = creatables
 				elif function_name in class_detections:
-					selected_completions.update(classes)
+					value = classes
+
+				if value is not None:
+					selected_completions.update([x for x in value if x.startswith(prefix)])
 			elif enum_match is not None and enum_match.end(0) >= row_col[1]:
 				if enum_match.group(2) is None or enum_match.group(2) == "":
 					selected_completions.update(enum_names)
